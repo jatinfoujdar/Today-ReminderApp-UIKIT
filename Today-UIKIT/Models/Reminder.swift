a0b9f4e3cd0d68101d47ea618256b5b440e36e4c
Jatin Foujdar <jatin321001@gmail.com>
2025-06-24 14:57:02 +0530

    static array of reminder model commit

--- Today-UIKIT/Models/Reminder.swift
+++ Today-UIKIT/Models/Reminder.swift
@@ -17,7 +17,18 @@ struct Reminder{
 
 
 #if DEBUG
-    extension Reminder {
-    
+extension Reminder {
+    static var sampleData: [Reminder] = [
+        Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(800.0), note: "Don't forget about taxi receipts"),
+        Reminder(title: "Code review", dueDate: Date().addingTimeInterval(14000.0), note: "Check tech specs in shared folder", complete: true),
+        Reminder(title: "Pick up new contacts", dueDate: Date().addingTimeInterval(24000.0), note: "Optometrist closes at 6:00PM"),
+        Reminder(title: "Add notes to retrospective", dueDate: Date().addingTimeInterval(3200.0), note: "Collaborate with project manager", complete: true),
+        Reminder(title: "Interview new project manager candidate", dueDate: Date().addingTimeInterval(60000.0), note: "Review portfolio"),
+        Reminder(title: "Mock up onboarding experience", dueDate: Date().addingTimeInterval(72000.0), note: "Think different"),
+        Reminder(title: "Review usage analytics", dueDate: Date().addingTimeInterval(83000.0), note: "Discuss trends with management"),
+        Reminder(title: "Confirm group reservation", dueDate: Date().addingTimeInterval(92500.0), note: "Ask about space heaters"),
+        Reminder(title: "Add beta testers to TestFlight", dueDate: Date().addingTimeInterval(101000.0), note: "v0.9 out on Friday")
+    ]
 }
 #endif
+
