a7b1d06efeea7495bc4c7bbeca57a712cc596d67
Jatin Foujdar <jatin321001@gmail.com>
2025-06-26 16:11:31 +0530

    configration of collection as a  list commit

---
+++ Today-UIKIT/ReminderListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Today-UIKIT
+//
+//  Created by jatin foujdar on 10/06/25.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration  = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
